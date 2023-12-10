c9ae8ff6c9662d403a23f9ef5fbb3eebcb1b0717
Золотарев Олег Вячеславович <ovzolotarev@gmail.com>
2023-12-10 18:52:27 +0300

    Исправление issue #1

--- src/commands_registry.cpp
+++ src/commands_registry.cpp
@@ -1,231 +1,208 @@
 /*
-	LightBaker3000 Frontend project,
-	(c) 2022 CrazyRussian
+    LightBaker3000 Frontend project,
+    (c) 2022 CrazyRussian
 */
 
-#include "common.h"
 #include "commands_registry.h"
+#include "common.h"
 #include "ui_common.h"
 
 #include "application.h"
-#include "text_utils.h"
-#include "imgui_internal.h"
 #include "imgui_helpers.h"
+#include "imgui_internal.h"
+#include "text_utils.h"
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-
-
 CCommandsRegistry::CCommandsRegistry()
 {
-	m_vecCommandDescriptor.clear();
+    m_vecCommandDescriptor.clear();
 }
 
 CCommandsRegistry::~CCommandsRegistry()
 {
-	ClearPointersVector(m_vecCommandDescriptor);
+    ClearPointersVector(m_vecCommandDescriptor);
 }
 
-void CCommandsRegistry::RegisterCommand(CCommand* pCommand)
+void CCommandsRegistry::RegisterCommand(CCommand *pCommand)
 {
-	m_vecCommandDescriptor.push_back(pCommand);
+    m_vecCommandDescriptor.push_back(pCommand);
 
-	if (pCommand->Flags() & CMD_ON_MAINTOOLBAR)
-		m_vecMainToolbarCommands.push_back(m_vecCommandDescriptor.size() - 1);
+    if (pCommand->Flags() & CMD_ON_MAINTOOLBAR)
+        m_vecMainToolbarCommands.push_back(m_vecCommandDescriptor.size() - 1);
 }
 
-std::vector<size_t>& CCommandsRegistry::GetMainToolbarCommands()
+std::vector<size_t> &CCommandsRegistry::GetMainToolbarCommands()
 {
-	return m_vecMainToolbarCommands;
+    return m_vecMainToolbarCommands;
 }
 
-CCommand* CCommandsRegistry::GetCommandByInternalIndex(int index)
+CCommand *CCommandsRegistry::GetCommandByInternalIndex(int index)
 {
-	return m_vecCommandDescriptor[index];
+    return m_vecCommandDescriptor[index];
 }
 
-CCommand* CCommandsRegistry::FindCommandByGlobalId(GlobalCommands id)
+CCommand *CCommandsRegistry::FindCommandByGlobalId(GlobalCommands id)
 {
-	for (auto& it : m_vecCommandDescriptor)
-	{
-		if (it->GetId() == id)
-			return it;
-	}
+    for (auto &it : m_vecCommandDescriptor)
+    {
+        if (it->GetId() == id)
+            return it;
+    }
 
-	return 0;
+    return 0;
 }
 
 void CCommandsRegistry::RenderCommandAsMenuItem(GlobalCommands cmdId, bool checked)
 {
-	CCommand* cmd = FindCommandByGlobalId(cmdId);
-	assert(cmd);
+    CCommand *cmd = FindCommandByGlobalId(cmdId);
+    assert(cmd);
 
-	bool tmp2 = checked;
+    bool tmp2 = checked;
 
-	if (ImGui::MenuItem(cmd->GetDescription(), cmd->GetShortcutDescription(),&tmp2))
-	{
-		cmd->Execute();
-	}
+    if (ImGui::MenuItem(cmd->GetDescription(), cmd->GetShortcutDescription(), &tmp2))
+    {
+        cmd->Execute();
+    }
 }
 
 bool CCommandsRegistry::OnKeyDown()
 {
-	auto kbState = SDL_GetKeyboardState(nullptr);
+    auto kbState = SDL_GetKeyboardState(nullptr);
 
-	for (auto& cmd : m_vecCommandDescriptor)
-	{
-		if (cmd->IsKeystrokeActive(kbState))
-		{
-			cmd->Execute();
-			return true;
-		}
-	}
+    for (auto &cmd : m_vecCommandDescriptor)
+    {
+        if (cmd->IsKeystrokeActive(kbState))
+        {
+            cmd->Execute();
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
-CCommand::CCommand(GlobalCommands id, const char* description,const char* keyStroke, GLTexture* icon, int flags, pfnCommandCallback callback) :
-	m_eCommandId(id),
-	m_pIcon(icon),
-	m_iFlags(flags),
-	m_pfnCallback(callback)
+CCommand::CCommand(GlobalCommands id, const char *description, const char *keyStroke, GLTexture *icon, int flags,
+                   pfnCommandCallback callback)
+    : m_eCommandId(id), m_pIcon(icon), m_iFlags(flags), m_pfnCallback(callback)
 {
-	strcpy_s(m_szDescription,description);
-	if (keyStroke)
-		strcpy_s(m_szKeyStroke, keyStroke);
-	else
-		*m_szKeyStroke = 0;
+    strcpy_s(m_szDescription, description);
+    if (keyStroke)
+        strcpy_s(m_szKeyStroke, keyStroke);
+    else
+        *m_szKeyStroke = 0;
 
+    for (auto &it : m_rKeys)
+        it = -1;
 
-	for (auto& it : m_rKeys)
-		it = -1;
+    if (keyStroke)
+    {
+        auto items = TextUtils::SplitTextSimple(keyStroke, strlen(keyStroke), '-');
 
-	if (keyStroke)
-	{ 
-		auto items = TextUtils::SplitTextSimple(keyStroke, strlen(keyStroke), '-');
+        auto addKey = [&](int keyId) -> bool {
+            int i = 0;
+            for (int i = 0; i < ARRAY_SIZE(m_rKeys); i++)
+            {
+                if (m_rKeys[i] == -1)
+                {
+                    m_rKeys[i] = keyId;
+                    return true;
+                }
+            }
 
-		auto addKey = [&](int keyId) -> bool
-		{
-			int  i = 0;
-			for (int i = 0; i < ARRAY_SIZE(m_rKeys); i++)
-			{
-				if (m_rKeys[i] == -1)
-				{
-					m_rKeys[i] = keyId;
-					return true;
-				}
-			}
+            return false;
+        };
 
-			return false;
-		};
+        auto it = items.begin();
 
-		auto it = items.begin();
+        while (it != items.end())
+        {
+            bool r = false;
 
-		while (it != items.end())
-		{
-			bool r = false;
+            auto scanCode = SDL_GetScancodeFromName((*it).c_str());
 
-			auto keyCode = SDL_GetKeyFromName((*it).c_str());
-			auto scanCode = SDL_GetScancodeFromKey(keyCode);
-
-			// Stupid workaround for RUS keyboard layout
-			if ((*it).c_str()[0] == '`')
-			{
-				keyCode = 1073742048;
-				scanCode = SDL_SCANCODE_GRAVE;
-			}
-
-			if (keyCode == SDLK_UNKNOWN)
-				Application::EPICFAIL("Bad shortcut for command \"%s\" - unknown key %s", m_szDescription, (*it).c_str());
-
-			if (scanCode == SDLK_UNKNOWN)
+            if (scanCode == SDLK_UNKNOWN)
             {
                 auto err = SDL_GetError();
                 Application::EPICFAIL("Bad shortcut for command \"%s\" - unknown key %s", m_szDescription,
                                       (*it).c_str());
             }
 
-            
+            r = addKey(scanCode);
 
-			r = addKey(scanCode);
+            if (!r)
+                Application::EPICFAIL("Bad shortcut for command \"%s\" - key stroke is too long", m_szDescription);
 
-			if (!r)
-				Application::EPICFAIL("Bad shortcut for command \"%s\" - key stroke is too long", m_szDescription);
-
-			it++;
-		}
-	}
-
-
+            it++;
+        }
+    }
 }
 
 void CCommand::Execute()
 {
-	m_pfnCallback();
+    m_pfnCallback();
 }
 
 void CCommand::RenderImGUI(int size)
 {
-	// ImGUI quirks
+    // ImGUI quirks
 #pragma warning(disable : 4312)
-	ImGui::SameLine();
+    ImGui::SameLine();
 
-	
+    if (m_iFlags & CMD_ONLY_ICON)
+    {
+        if (ImGui::ImageButton(m_szDescription, (ImTextureID)m_pIcon->GLTextureNum(), ImVec2(size, size)))
+            Execute();
+    }
+    else
+    {
+        auto style   = ImGui::GetStyle();
+        auto bgColor = style.Colors[ImGuiCol_Button];
 
-	if (m_iFlags & CMD_ONLY_ICON)
-	{
-		if (ImGui::ImageButton(m_szDescription, (ImTextureID)m_pIcon->GLTextureNum(), ImVec2(size, size)))
-			Execute();
-	}
-	else
-	{
-		auto style = ImGui::GetStyle();
-		auto bgColor = style.Colors[ImGuiCol_Button];
-		
-
-		//if (ImGui::ImageButton(m_szDescription, (ImTextureID)m_pIcon->gl_texnum, ImVec2(size, size)))
-		if (ImGuiHelpers::ImageButtonWithText((ImTextureID)m_pIcon->GLTextureNum(), m_szDescription, ImVec2(size, size), ImVec2(1, 1), ImVec2(0, 0), style.FramePadding.x, bgColor, ImVec4(1, 1, 1, 1)))
-		{
-			Execute();
-		}
-	}
+        // if (ImGui::ImageButton(m_szDescription, (ImTextureID)m_pIcon->gl_texnum, ImVec2(size, size)))
+        if (ImGuiHelpers::ImageButtonWithText((ImTextureID)m_pIcon->GLTextureNum(), m_szDescription, ImVec2(size, size),
+                                              ImVec2(1, 1), ImVec2(0, 0), style.FramePadding.x, bgColor,
+                                              ImVec4(1, 1, 1, 1)))
+        {
+            Execute();
+        }
+    }
 #pragma warning(restore : 4312)
 }
 
 GlobalCommands CCommand::GetId()
 {
-	return m_eCommandId;
+    return m_eCommandId;
 }
 
-const char* CCommand::GetDescription()
+const char *CCommand::GetDescription()
 {
-	return m_szDescription;
+    return m_szDescription;
 }
 
-bool CCommand::IsKeystrokeActive(const uint8_t* kbState)
+bool CCommand::IsKeystrokeActive(const uint8_t *kbState)
 {
-	if (m_rKeys[0] == -1) // No hotkey assigned
-		return false;
+    if (m_rKeys[0] == -1) // No hotkey assigned
+        return false;
 
-	bool bValidStroke = true;
+    bool bValidStroke = true;
 
-	for (auto& key : m_rKeys)
-	{
-		if (key == -1) // Reached end of key combo
-			break;
+    for (auto &key : m_rKeys)
+    {
+        if (key == -1) // Reached end of key combo
+            break;
 
-		if (!kbState[key])
-		{
-			bValidStroke = false;
-			break;
-		}
+        if (!kbState[key])
+        {
+            bValidStroke = false;
+            break;
+        }
+    }
 
-	}
-	
-	return bValidStroke;
+    return bValidStroke;
 }
 
-const char* CCommand::GetShortcutDescription()
+const char *CCommand::GetShortcutDescription()
 {
-	return m_szKeyStroke;
+    return m_szKeyStroke;
 }
